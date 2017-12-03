1863588ff02997a0d09738d9e352c0e94ad3f467
shiba123git <shibaprasadm657@gmail.com>
2017-12-03 14:52:27 +0530

    Contact Details

---
+++ src/main/java/com/dsms/solutions/tfnecu/common/ContactDetails.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.dsms.solutions.tfnecu.common;
+
+/**
+ * @author Babu
+ *
+ */
+public class ContactDetails {
+
+	/**
+	 * 
+	 */
+	public ContactDetails() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
